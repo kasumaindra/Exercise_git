5b1b76bc508fbda6956ea0a9dbcde095af34d3cc
kasumaindra <59867434+kasumaindra@users.noreply.github.com>
2024-03-20 16:17:11 +0700

    add new sout

--- src/student.java
+++ src/student.java
@@ -4,5 +4,6 @@ public class student {
     public static void main(String[] args) {
         System.out.println("Student");
         System.out.println("Student");
+        System.out.println("Student");
     }
 }
